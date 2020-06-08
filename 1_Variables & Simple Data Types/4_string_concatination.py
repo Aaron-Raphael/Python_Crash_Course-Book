c7e26f119dc443fd7bda4525a10f5cfb9f4efd89
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-08 18:24:08 +0530

    4_string_concatination

---
+++ 1_Variables & Simple Data Types/4_string_concatination.py
@@ -0,0 +1,9 @@
+first_name = 'aaron'
+last_name = 'raphael'
+
+full_name = first_name + ' ' + last_name    # varibles and strings are combined using + symbol
+
+#print('Helllo, ' + full_name.title() + ' !')
+
+message = 'Helllo, ' + full_name.title() + ' !'
+print(message)
