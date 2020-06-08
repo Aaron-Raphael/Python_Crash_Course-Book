d87fbc1d2571184db22546d953217b898b11f066
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-08 18:19:57 +0530

    2_message_variable_edit

---
+++ 1_Variables & Simple Data Types/2_message_variable_edit.py
@@ -0,0 +1,5 @@
+message = 'Hello, Good morning'     # string stored in variable 
+print(message)                      # string in the variable in printed
+
+message = 'Hello, Good afternoon'   # string in variable is changed
+print(message)                      # new string in the variable is printed
