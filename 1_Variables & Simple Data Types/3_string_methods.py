f1a1449f72312bb760b6052e53ae22c6a52b5d6d
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-08 18:22:57 +0530

    3_string_method

---
+++ 1_Variables & Simple Data Types/3_string_methods.py
@@ -0,0 +1,8 @@
+name = 'aaron raphael'  # name stored in variable
+
+print(name.title())     # title() method displays string in titlecase
+print(name.upper())     # upper() method displays string in upper case
+print(name.lower())     # lower() method displays string in lower case
+
+""" Every method is followed by a set of parentheses
+    to provide aditional info if needed """
