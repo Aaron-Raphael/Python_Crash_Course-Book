fe9110eec82e8457d499b0f4479696d05ae036aa
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-10 17:28:50 +0530

    1_lists

---
+++ 2_Lists/1_lists.py
@@ -0,0 +1,10 @@
+# in python [] represent list
+# list elements are seperated by ' , '
+
+names = ['arthur' , 'john' , 'tommy']
+print(names)
+
+# numerical list using range ..end val not inc
+#          range(start , end , increment value[opt])
+num = list(range(1,10,2))
+print(num)
