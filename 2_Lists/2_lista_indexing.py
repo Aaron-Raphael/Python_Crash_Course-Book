4e470e7b074d99ae5ee1c84bbd3365ce77fced60
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-10 17:30:44 +0530

    2_list_indexing

---
+++ 2_Lists/2_lista_indexing.py
@@ -0,0 +1,10 @@
+# items in list can be accessed by indexing 
+# [] is used to index and starts from 0
+
+names = ['arthur' , 'john' , 'tommy']
+elder = f"{names[0].title()} is the elder brother"
+print(elder)
+
+# negative indexing is done starting from -1
+youngest = f"{names[-1].title()} is the youngest brother"
+print(youngest) 
