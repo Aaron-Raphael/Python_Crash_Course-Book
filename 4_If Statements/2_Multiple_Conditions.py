d6ccbeefb480db4ea9ce098aa7aedce7e32a39d0
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-13 21:40:33 +0530

    2_Multiple_Conditions

---
+++ 4_If Statements/2_Multiple_Conditions.py
@@ -0,0 +1,11 @@
+day = 'sunny'
+mood = 'good'
+
+# and returns True if both conditions are True
+# or returns True if either one is True
+
+if day == 'sunny' and mood == 'good':
+    print('its a great day')
+
+if day == 'rainy' or mood == 'good':
+    print('enjoy the day')
