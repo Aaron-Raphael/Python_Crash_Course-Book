86aa074c0f4ad1e47987c2e3c3bccfbe695e62b6
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-16 18:27:27 +0530

    2_Odd_or_Even_Modulo

---
+++ 6_User Input & While Loops/2_Odd_or_Even_Modulo.py
@@ -0,0 +1,9 @@
+# modulo % gives the remainder of the number
+
+num = int(input('enter number to check if odd or even : '))
+
+if num % 2 == 0:
+    print(num, 'is a even num')
+
+else:
+    print(num, 'is a odd num')
