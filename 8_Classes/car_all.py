998479af1eab1ea286d231cba73ecf69f44a8104
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-24 07:32:38 +0530

    car_all

---
+++ 8_Classes/car_all.py
@@ -0,0 +1,61 @@
+class Car():
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading =0    # default values can set to an attribute
+    
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+        
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage): # defining method to update value
+        """Set the odometer reading to the given value."""
+        if mileage>= self.odometer_reading:
+            self.odometer_reading = mileage
+
+    def increment_odometer(self, miles): # method to increment odometer  reading
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles 
+
+    def fill_gas_tank(self):
+        print("you have enough gas for today")
+
+
+# creating inherited class
+
+# instance as classes
+# you  can break the code and define in seperate class and include in code
+class Assistant():
+    """attempt to model a assistant"""
+    def __init__(self , assistant_version = 2.0):
+        """initialise assistant attribute"""
+        self.assistant_version = assistant_version
+
+    def describe_assistant(self):
+        print('the A.I assistant is version', self.assistant_version)
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year) # super function makes connection between child and parent class
+
+        self.battery_size = 70 # defining attribute for child class
+
+        self.assistant = Assistant() # defining external class as attribute
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def fill_gas_tank(self):   #   overriding methods from the parent class 
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
